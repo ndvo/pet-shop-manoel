8415cd76610021fa7cbe2ebfc4c0a13424c1ad1f
Manoel <manoelmndf@gmail.com>
2024-12-18 14:46:11 -0300

    Javascript in another file separated from the html file

--- js/product.js
+++ js/product.js
@@ -1,18 +1,20 @@
-const template = document.querySelector('#product-template');
-const tableContainer = document.querySelector('#product-div-table');
+  const template = document.querySelector('#product-template');
+  const productContainer = document.querySelector('#product-container');
 
-const data = [
-  { product: 'Dog Food', price: $100 },
-  { product: 'Cat Food', price: $50 },
-  { product: 'Rat Food', price: $10 },
-  { product: 'Fish Food', price: $1 },
-  { product: 'Dog House', price: $100 },
-  { product: 'Aquarium', price: $500 },
-];
+  const data = [
+  {image: 'dogfood.jpg',  product: 'Dog Food',  price: "$100" },
+  {image: 'catfood.jpg',  product: 'Cat Food',  price: "$50" },
+  {image: 'ratfood.jpg',  product: 'Rat Food',  price: "$10" },
+  {image: 'fishfood.jpg', product: 'Fish Food', price: "$1" },
+  {image: 'doghouse.jpg', product: 'Dog House', price: "$100" },
+  {image: 'aquarium.jpg', product: 'Aquarium',  price: "$500" }
+  ];
 
-data.forEach(item => {
-  const row = template.content.cloneNode(true);
-  row.querySelector('.cell:nth-child(1)').textContent = item.product;
-  row.querySelector('.cell:nth-child(2)').textContent = item.price;
-  tableContainer.appendChild(row);
-});
+        data.forEach(item => {
+            const row = template.content.cloneNode(true);
+  row.querySelector('.card-img-top').setAttribute('src', `img/${item.image}`);
+  row.querySelector('.card-title').textContent = item.product;
+  row.querySelector('.card-text').textContent = item.price;
+  productContainer.appendChild(row);
+        });
+
