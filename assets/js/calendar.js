6dabdea878637b3e4f3f2823e82d44b1dd9f5afc
Manoel <manoelmndf@gmail.com>
2024-12-20 14:51:41 -0300

    docs: comments removed.

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,36 +1,31 @@
-// Função para redirecionar ao calendário
+
 window.onload = function () {
   renderCalendar();
 
 }
 
-// Variáveis para o estado atual
+
 let currentDate = new Date();
 
 function renderCalendar() {
   const calendar = document.querySelector('.calendar');
   const currentMonth = document.getElementById('current-month');
 
-  // Limpa os dias anteriores
   calendar.querySelectorAll('.calendar-cell').forEach(cell => cell.remove());
 
-  // Configura o mês e o ano
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth();
   currentMonth.textContent = `${currentDate.toLocaleString('default', { month: 'long' })} ${year}`;
 
-  // Primeiro dia do mês e número de dias
   const firstDay = new Date(year, month, 1).getDay();
   const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-  // Preenchendo com células em branco antes do primeiro dia
   for (let i = 0; i < firstDay; i++) {
     const blankCell = document.createElement('div');
     blankCell.classList.add('calendar-cell');
     calendar.appendChild(blankCell);
   }
 
-  // Preenchendo os dias do mês
   for (let day = 1; day <= daysInMonth; day++) {
     const dayCell = document.createElement('div');
     dayCell.classList.add('calendar-cell');
@@ -39,7 +34,6 @@ function renderCalendar() {
   }
 }
 
-// Navegação entre meses
 document.getElementById('prev-month').addEventListener('click', () => {
   currentDate.setMonth(currentDate.getMonth() - 1);
   renderCalendar();
--- assets/js/loadcomponents.js
+++ assets/js/loadcomponents.js
@@ -1,13 +1,13 @@
 function loadComponent(url, placeholderId) {
     fetch(url)
         .then(response => {
-            if (!response.ok) throw new Error(`Não foi possível carregar ${url}`);
+            if (!response.ok) throw new Error(`This page could not be loaded ${url}`);
             return response.text();
         })
         .then(data => {
             document.getElementById(placeholderId).innerHTML = data;
         })
-        .catch(error => console.error(`Erro ao carregar ${url}:`, error));
+        .catch(error => console.error(`Error loading this page ${url}:`, error));
 }
 
 document.addEventListener("DOMContentLoaded", function () {
