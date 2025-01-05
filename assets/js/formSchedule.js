b4825635b8cabd7aed590fcca7df3ce93921d0f7
Manoel Messias <manoelmndf@gmail.com>
2025-01-04 22:15:58 -0300

    create form page, get parameters and local storage

---
+++ assets/js/formSchedule.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('appointmentForm');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+
+        const appointment = {
+            year: new URLSearchParams(window.location.search).get('year'),
+            month: new URLSearchParams(window.location.search).get('month'),
+            day: new URLSearchParams(window.location.search).get('day'),
+            hour: new URLSearchParams(window.location.search).get('hour'),
+            ownerName: formData.get('ownerName'),
+            petName: formData.get('petName'),
+            petSpecies: formData.get('petSpecies')
+        };
+
+        // Salvar no localStorage
+        const appointments = JSON.parse(localStorage.getItem('appointments')) || [];
+        appointments.push(appointment);
+        localStorage.setItem('appointments', JSON.stringify(appointments));
+
+        alert('Appointment successfully booked!');
+
+        window.location.href = 'index.html';
+    });
+});
