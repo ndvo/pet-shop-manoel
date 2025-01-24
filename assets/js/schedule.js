5f299722707e8e09ce06eae73bf21ea7289208d0
Manoel Messias <manoelmndf@gmail.com>
2025-01-24 11:21:12 -0300

    Feature/20 navigation buttons (#25)
    * pass date as parameter
    
    * create time page and get parameters
    
    * create form page, get parameters and local storage
    
    * refactor: delete line unnecessary in head section
    
    * Update formSchedule.js
    
    * Update assets/js/schedule.js
    
    Co-authored-by: Nelson do Vale Oliveira <nelson@ocastudios.com>
    
    * bug fix
    
    * bug fix month
    
    * deleted comment
    
    * permissions review
    
    * Add admin menu maintain product (#18)
    
    * enable menu admin
    
    * check file and directory permissions
    
    * formatting css - menu and index
    
    * formatting css - maintain products
    
    * button schedule another date
    
    ---------
    
    Co-authored-by: Nelson do Vale Oliveira <nelson@ocastudios.com>

---
+++ assets/js/schedule.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const selectedDay = urlParams.get('day');
+    const selectedMonth = urlParams.get('month');
+    const selectedYear = urlParams.get('year');
+
+    const dateElement = document.getElementById('selected-date');
+    dateElement.textContent = `${selectedDay}/${selectedMonth}/${selectedYear}`;
+
+    const scheduleGrid = document.querySelector('.schedule-grid');
+
+    for (let hour = 8; hour <= 18; hour++) {
+        const hourBlock = createHourBlock(hour, selectedDay, selectedMonth, selectedYear);
+        scheduleGrid.appendChild(hourBlock);
+    }
+
+    addButtonSchedule();
+
+    function addButtonSchedule() {
+        const button = document.getElementById('scheduleButton');
+        if (button) {
+            button.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                const url = `appointment.html?day=${selectedDay}&month=${selectedMonth}&year=${selectedYear}`;
+
+                window.location.href = url;
+            });
+        } else {
+            console.error('Elemento com ID "scheduleButton" não encontrado.');
+        }
+    }
+});
+
+function createHourBlock(hour, selectedDay, selectedMonth, selectedYear) {
+    const hourBlock = document.createElement('div');
+    hourBlock.classList.add('hour-block', 'p-3', 'border', 'text-center', 'mb-2');
+    hourBlock.textContent = `${hour}:00 - ${hour + 1}:00`;
+
+    hourBlock.addEventListener('click', () => {
+        window.location.href = `formSchedule.html?day=${selectedDay}&month=${selectedMonth}&year=${selectedYear}&hour=${hour}`;
+    });
+    return hourBlock;
+};
